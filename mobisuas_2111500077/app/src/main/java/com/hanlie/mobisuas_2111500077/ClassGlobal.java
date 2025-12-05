e27ece7a39b35b540fe89b1e5e3cf9e254922f87
johan99999 <achanferguso@gmail.com>
2025-12-05 13:15:03 +0700

    [ADD] Menambahkan halaman login

--- mobisuas_2111500077/app/src/main/java/com/hanlie/mobisuas_2111500077/ClassGlobal.java
+++ mobisuas_2111500077/app/src/main/java/com/hanlie/mobisuas_2111500077/ClassGlobal.java
@@ -4,10 +4,9 @@ import android.app.Application;
 public class ClassGlobal extends Application {
     public static String global_ipaddress="http://10.0.2.2";
     private static String URL=global_ipaddress+"/webmobisuas_2111500077/android/";
-    public String getUrl() {
-        return URL;
-    }
-    public void setURL (String url) {
-        URL = url;
-    }
+    public String getUrl() { return URL; }
+    public void setURL (String url) { URL = url; }
+    public static String global_gambar="gambar";
+    public static Integer global_idlaporan=0;
+    public static Integer global_iduser=0;
 }
--- mobisuas_2111500077/app/src/main/java/com/hanlie/mobisuas_2111500077/SplashScreen.java
+++ mobisuas_2111500077/app/src/main/java/com/hanlie/mobisuas_2111500077/SplashScreen.java
@@ -21,7 +21,7 @@ public class SplashScreen extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), e.getMessage(),
                             Toast.LENGTH_LONG).show();
                 } finally {
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    Intent intent = new Intent(getApplicationContext(), Login.class);
                     startActivity(intent);
                     finish();
                 }
